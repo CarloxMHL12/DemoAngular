2d4d0a2277a25c58babfc3537e00809098322dfc
CarloxMHL12 <carlosmiguelhl@outlook.com>
2026-04-09 13:30:44 -0600

    Se agregaron validaciones en los campos del formulario.

--- demoangular.client/src/components/modal/modal.component.ts
+++ demoangular.client/src/components/modal/modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Usuario } from '../../models/usuario';
 
 @Component({
@@ -7,21 +8,32 @@ import { Usuario } from '../../models/usuario';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
 
-  usuario: Usuario = {
-    id: 0,
-    nombre: '',
-    apellidoPaterno: '',
-    apellidoMaterno: '',
-    telefono: '',
-    correo: '',
-    username: ''
-  };
+  usuarioForm!: FormGroup;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
-    if (data.modo === 'editar') {
-      this.usuario = { ...data.usuario };
-    }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {
+    const usuario: Usuario = this.data.usuario || {
+      id: 0,
+      nombre: '',
+      apellidoPaterno: '',
+      apellidoMaterno: '',
+      telefono: '',
+      correo: '',
+      username: ''
+    };
+
+    this.usuarioForm = new FormGroup({
+      nombre: new FormControl(usuario.nombre, [Validators.required, Validators.minLength(5)]),
+      apellidoPaterno: new FormControl(usuario.apellidoPaterno, Validators.required),
+      apellidoMaterno: new FormControl(usuario.apellidoMaterno),
+      telefono: new FormControl(usuario.telefono, [Validators.required, Validators.pattern('^[0-9]{10}$')]),
+      correo: new FormControl(usuario.correo, [Validators.required, Validators.email]),
+      username: new FormControl(usuario.username, [Validators.required, Validators.minLength(4)])
+    });
   }
+
+  get f() { return this.usuarioForm.controls; }
 }
